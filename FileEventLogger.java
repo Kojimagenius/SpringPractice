4838159c2a3aa19dac1a90dbecebc14ad4c21f2d
Max <dun.max@yandex.ru>
2019-01-22 19:55:32 +0300

    Added file logger

--- FileEventLogger.java
+++ FileEventLogger.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileEventLogger implements EventLogger {
-    String fileName;
-    File file;
+    private String fileName;
+    private File file;
 
     public void Init() throws IOException{
         this.file = new File(fileName);
